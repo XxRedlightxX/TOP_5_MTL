07aee80597d9f33d617a9d5319e6a83333b5c81f
Ashes <105072051+ashes200@users.noreply.github.com>
2024-10-03 13:45:37 -0400

    lang 0.3

--- FrontEnd/src/components/FooterComponent.vue
+++ FrontEnd/src/components/FooterComponent.vue
@@ -1,15 +1,39 @@
 <template>
     <div id="footerComponent">
         <div class="links">
-            <router-link to="/">Home</router-link>
-            <router-link to="/Events">Events</router-link>
-            <router-link to="/Profile">Profile</router-link>
+            <router-link to="/" :title="actualLang ? 'Go to Home Page' : 'Allez a la page d\'accueil'">{{actualLang ? 'Home' : 'Accueil'}}</router-link>
+            <router-link to="/Events" :title="actualLang ? 'Go to Event Page' : 'Allez a la page d\'aEvenement'">{{actualLang ? 'Events' : 'Evenement'}}</router-link>
+            <router-link to="/Profile" :title="actualLang ? 'Go to Profile Page' : 'Allez a la page Profile'">{{actualLang ? 'Profile' : 'Profile'}}</router-link>
         </div>
 
         <p>&copy Footer Component</p>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+    import storageManager from "@/JS/LocalStaorageManager"
+    import { ref, onMounted, onUnmounted} from "vue";
+
+    let actualLang = ref(storageManager.getLang());
+
+    if (actualLang.value === null) {
+        storageManager.setLang(true);
+        actualLang.value = storageManager.getLang();
+    }
+
+    // Function to handle mode change event
+    const handleLangChange = (event) => {
+        actualLang.value = JSON.parse(event.detail.storage);
+    };
+    // Add event listener for mode changes
+    onMounted(() => {
+        window.addEventListener('lang-changed', handleLangChange);
+    });
+
+    // Remove event listener when component is unmounted
+    onUnmounted(() => {
+        window.removeEventListener('lang-changed', handleLangChange);
+    });
+</script>
 
 <style src="../styles/FooterComponentStyle.scss"></style>
--- FrontEnd/src/components/HomeComponents/ContactComponent.vue
+++ FrontEnd/src/components/HomeComponents/ContactComponent.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="contactComponent">
         <div class="header">
-            <h3>Contact Us</h3>
+            <h3 v-if="actualLang">Contact Us</h3>
+            <h3 v-else>Contacter nous</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta facilis enim quas laboriosam consequuntur 
                 itaque veritatis, officiis consequatur, quidem illum, ad saepe quos. Nobis, repudiandae molestiae! Obcaecati 
                 nihil beatae veritatis?</p>
@@ -13,7 +14,8 @@
                 <div>
                     <v-icon icon="mdi-map-marker" class="icon glow"/>
                     <div>
-                        <h4>Address</h4>
+                        <h4 v-if="actualLang">Address</h4>
+                        <h4 v-else>Adresse</h4>
                         <p>0000 rue Montreal, A0A 0A0, Qc, CA</p>
                     </div>
                 </div>
@@ -21,7 +23,8 @@
                 <div>
                     <v-icon icon="mdi-phone" class="icon glow"/>
                     <div>
-                        <h4>Phone</h4>
+                        <h4 v-if="actualLang">Phone</h4>
+                        <h4 v-else>Telephone</h4>
                         <p>(000) 000 0000</p>
                     </div>
                 </div>
@@ -37,23 +40,24 @@
             </div>
             <div class="right">
                 <form action="#" method="post">
-                    <h3>Send Message</h3>
+                    <h3 v-if="actualLang">Send Message</h3>
+                    <h3 v-else>Envoyez votre message</h3>
 
                     <v-text-field
                         :rules="Name"
                         hide-details="auto"
-                        label="Full name"
+                        :label="actualLang ? 'Full name' : 'Nom complet'"
                     ></v-text-field>
 
                     <v-text-field
                         :rules="Email"
                         hide-details="auto"
-                        label="Email"
+                        :label="actualLang ? 'Email' : 'Email'"
                     ></v-text-field>
 
-                    <v-textarea label="Type your Message"></v-textarea>
+                    <v-textarea :label="actualLang ? 'Type your Message' : 'Entrez votre message'"></v-textarea>
 
-                    <input type="button" value="Send">
+                    <input type="button" :value="actualLang ? 'Send' : 'Envoyer'">
 
                 </form>
             </div>
@@ -61,6 +65,31 @@
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+    import storageManager from "@/JS/LocalStaorageManager"
+    import { ref, onMounted, onUnmounted} from "vue";
+
+    let actualLang = ref(storageManager.getLang());
+
+    if (actualLang.value === null) {
+        storageManager.setLang(true);
+        actualLang.value = storageManager.getLang();
+    }
+
+    // Function to handle mode change event
+    const handleLangChange = (event) => {
+        actualLang.value = JSON.parse(event.detail.storage);
+    };
+    // Add event listener for mode changes
+    onMounted(() => {
+        window.addEventListener('lang-changed', handleLangChange);
+    });
+
+    // Remove event listener when component is unmounted
+    onUnmounted(() => {
+        window.removeEventListener('lang-changed', handleLangChange);
+    });
+    
+</script>
 
 <style src="../../styles/HomesStyles/ContactComponentStyle.scss"></style>
