5de5ea274b928f039bf6570da7834c20042bacd5
Gabriel <2016069@crosemont.qc.ca>
2024-10-07 19:05:12 -0400

    mauvais design de la liste events (A Voir)

--- FrontEnd/src/components/EventComponents/EventFilterComponent.vue
+++ FrontEnd/src/components/EventComponents/EventFilterComponent.vue
@@ -1,6 +1,67 @@
 <template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div id="eventFilterComponent">
-        <h1>Event Filter Component</h1>
+
+       
+        <div class="categories">
+            <div class="categories_horizontal">
+                <div style="width: 50%;  float: left; "> 
+                    <h2>Categories</h2>
+                </div>
+                <div style="margin-left: 85%;  "> 
+                    <div class="search-container" >
+                            <button type="submit" class="search-icon">
+                                <i class="fa fa-search"></i>
+                            </button>
+                            <input type="text" placeholder="Search..." class="search-bar" >
+                    </div>
+                </div>
+            </div>
+            <div class="listeCategories">
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg" class="filter-green">
+                    Gaming
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Music
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Arts
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Biking
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Hiking
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Internship
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Internship
+                </div>
+
+                <div class="listeCategories_objet">
+                    <img src="/src/assets/Logo-Gem.svg">
+                    Internship
+                </div>
+
+                
+                
+            </div>
+        </div>
     </div>
 </template>
 
