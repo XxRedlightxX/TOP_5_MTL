1a16b79da83e62c6226a059468df757bf20d5edd
Gabriel <2016069@crosemont.qc.ca>
2026-01-30 15:56:59 -0500

    change

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -16,10 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-        UserSeeder::class,
         TypeSeeder::class,
         SaisonSeeder::class,
-        ActiviteSeeder::class,
     ]);
 
         User::factory()->create([
