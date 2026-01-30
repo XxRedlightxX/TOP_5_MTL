0412ae80ab2df2f0b85650591f263b3389296292
Gabriel <2016069@crosemont.qc.ca>
2026-01-29 22:54:09 -0500

    Adding dataSeeder in backend for backend migration

--- Backend/database/seeders/TypeSeeder.php
+++ Backend/database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+        ['nom' => 'Concert', 'image_data' => 'mdi-music'],
+        ['nom' => 'Festival', 'image_data' => 'mdi-party-popper'],
+        ['nom' => 'Sport', 'image_data' => 'mdi-soccer'],
+        // Add the rest from your SQL...
+    ];
+
+    foreach ($types as $type) {
+       Type::create($type);
+    }
     }
 }
