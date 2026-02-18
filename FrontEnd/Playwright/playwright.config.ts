7652d1adaf9c534b6314d6198088e5266fb66679
Gabriel <2016069@crosemont.qc.ca>
2026-02-18 16:10:34 -0500

    test 4

--- FrontEnd/Playwright/playwright.config.ts
+++ FrontEnd/Playwright/playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-
+require('dotenv').config({ path: '../.env' });
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-     baseURL: process.env.APP_URL || 'http://localhost:3000',
+     baseURL: process.env.APP_URL ,
 
        // automatically use logged-in state
 
